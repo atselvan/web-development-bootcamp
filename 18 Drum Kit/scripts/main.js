ba5597e3a17f82c744b1891a5997f8b9901b6339
Allan Selvan <allantony2008@gmail.com>
2024-02-18 11:57:09 +0100

    Added javascript dom manipulation exercises

---
+++ 18 Drum Kit/scripts/main.js
@@ -0,0 +1,55 @@
+var wAudio = new Audio("sounds/tom1.mp3");
+var aAudio = new Audio("sounds/tom2.mp3");
+var sAudio = new Audio("sounds/tom3.mp3");
+var dAudio = new Audio("sounds/tom4.mp3");
+var jAudio = new Audio("sounds/snare.mp3");
+var kAudio = new Audio("sounds/crash.mp3");
+var lAudio = new Audio("sounds/kick.mp3");
+
+var numberOfDrums = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numberOfDrums; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    drumButtonInnerHTML = this.innerHTML;
+    playDrum(drumButtonInnerHTML);
+  });
+}
+
+document.addEventListener("keydown", function (event) {
+  playDrum(event.key)
+});
+
+function playDrum(key) {
+  switch (key) {
+    case "w":
+      wAudio.play();
+      wAudio.currentTime = 0;
+      break;
+    case "a":
+      aAudio.play();
+      aAudio.currentTime = 0;
+      break;
+    case "s":
+      sAudio.play();
+      sAudio.currentTime = 0;
+      break;
+    case "d":
+      dAudio.play();
+      dAudio.currentTime = 0;
+      break;
+    case "j":
+      jAudio.play();
+      jAudio.currentTime = 0;
+      break;
+    case "k":
+      kAudio.play();
+      kAudio.currentTime = 0;
+      break;
+    case "l":
+      lAudio.play();
+      lAudio.currentTime = 0;
+      break;
+    default:
+      break;
+  }
+}

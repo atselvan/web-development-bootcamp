9fd823a21483a5f59e015cc927bead023b2cbe96
Allan Selvan <allantony2008@gmail.com>
2024-03-24 10:33:21 +0100

    added custom middleware

--- 24.2 Express Middleware/index.js
+++ 24.2 Express Middleware/index.js
@@ -1,11 +1,20 @@
 import express from "express";
-import { dirname } from "path";
+import { dirname, join } from "path";
 import { fileURLToPath } from "url";
 import bodyParser from "body-parser";
+import morgan from "morgan";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
+
+function authMiddleware(req, res, next) {
+  console.log("Authenticated successfully")
+  next();
+}
+
+app.use(morgan("combined"));
+app.use(authMiddleware);
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
@@ -15,6 +24,7 @@ app.use(bodyParser.urlencoded({extended: true}))
 
 app.post("/submit", (req, res) => {
   console.log(req.body)
+  res.send("Success")
 })
 
 app.listen(port, () => {

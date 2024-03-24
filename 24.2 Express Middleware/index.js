4cf49461f0baa4f26edc5b3951c3053c9a77b916
Allan Selvan <allantony2008@gmail.com>
2024-03-24 11:17:03 +0100

    added submit response

--- 24.2 Express Middleware/index.js
+++ 24.2 Express Middleware/index.js
@@ -23,8 +23,8 @@ app.get("/", (req, res) => {
 app.use(bodyParser.urlencoded({extended: true}))
 
 app.post("/submit", (req, res) => {
-  console.log(req.body)
-  res.send("Success")
+  var body = req.body
+  res.send(`<H1>Your band name is:</H1><H2>"${body.street}${body.pet} 😀"</H2>`)
 })
 
 app.listen(port, () => {
